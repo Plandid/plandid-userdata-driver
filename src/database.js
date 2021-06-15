c230456b2d76083ab5f79a5deee5cae0db7e1d61
Carl Schader <carlschader@gmail.com>
2021-06-15 14:58:04 -0600

    added jest for unit testing and should be blocking commits if tests pass

--- src/database.js
+++ src/database.js
@@ -4,7 +4,6 @@ const { databaseName } = require("./config");
 const client = new MongoClient(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = {
-    db: client.db(databaseName),
     connect: async function() {
         try {
             await client.connect();
@@ -13,5 +12,17 @@ module.exports = {
             console.error(error);
             return false;
         }
+    },
+    disconnect: async function() {
+        try {
+            await client.close();
+            return true;
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    },
+    fetchdb: function() {
+        return client.db(databaseName);
     }
 };
---
+++ src/utils.test.js
@@ -0,0 +1,5 @@
+const utils = require("./utils");
+
+test("testing jest and only allowing commits if tests pass", function() {
+    expect(true).toBe(false);
+});
