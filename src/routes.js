97fa00c34db9233e8ae454e2332539e9f9fb9416
CarlSchader <carlwschader@gmail.com>
2021-06-26 17:05:37 -0600

    changed the paths in routes.js to use routes instead of resources

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-// router.use("/accounts", require("./resources/accounts"));
-// router.use("/pending-accounts", require("./resources/pendingAccounts"));
-// router.use("/online-accounts", require("./resources/onlineAccounts"));
-// router.use('/schedules', require('./resources/schedules'));
-// router.use("/stripeCustomers", require("./resources/stripeCustomers"));
+router.use("/accounts", require("./routes/accounts"));
+router.use("/pending-accounts", require("./routes/pendingAccounts"));
+router.use("/online-accounts", require("./routes/onlineAccounts"));
+router.use('/schedules', require('./routes/schedules'));
+router.use("/stripeCustomers", require("./routes/stripeCustomers"));
 
-// router.use("/:schedule/people", require("./resources/people"));
-// router.use("/:schedule/events", require("./resources/events"));
-// router.use("/:schedule/categories", require("./resources/categories"));
-// router.use('/:schedule/plans', require('./resources/plans'));
+router.use("/:schedule/people", require("./routes/people"));
+router.use("/:schedule/events", require("./routes/events"));
+router.use("/:schedule/categories", require("./routes/categories"));
+router.use('/:schedule/plans', require('./routes/plans'));
 
 module.exports = router;
